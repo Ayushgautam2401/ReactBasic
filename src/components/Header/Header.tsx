dad91725aefeb1a7f1acb5ced2635c2f731afd7b
Ayushgautam2401 <ayush.gautam@biztecno.net>
2024-06-14 15:56:05 +0530

    some Changes in headers

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,16 +8,16 @@ function Header() {
 		<div className='flex flex-row mainHeader width-100 flex-justify-between'>
 
 			<div className='flex flex-row padding-2 flex-align-center padding-l-8'>
-				<div className='logoElips padding-1 width-100'>
+				<div className='logoElips width-100'>
 					<img src={logo} alt='logo' />
 				</div>
-				<div className='padding-l-2'>Retro</div>
+				<div className='text-bold padding-l-2'>Retro</div>
 			</div>
 
 			<div className='flex flex-row flex-align-center padding-r-8'>
-				<div>Home</div>
-				<div>About Me</div>
-				<div>Contact Me</div>
+				<div className='text-bold'>Home</div>
+				<div className='text-bold padding-r-4 padding-l-4'>About Me</div>
+				<div className='text-bold'>Contact Me</div>
 			</div>
 
 		</div>
